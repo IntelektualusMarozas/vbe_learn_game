5a5cb2b00878dbbc379045ddb510c8a5588f1cab
IntelektualusMarozas <juozassadauskas2@gmail.com>
2025-08-07 21:20:17 +0300

    feat: main menu

---
+++ MainMenu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+func _ready():
+	$VBoxContainer/StartButton.pressed.connect(_on_start_button_pressed)
+	$VBoxContainer/Options.pressed.connect(_on_options_button_pressed)
+	$VBoxContainer/Exit.pressed.connect(_on_exit_button_pressed)
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://ExamPage.tscn")
+
+func _on_options_button_pressed():
+	print("Options, TBU")
+	pass
+
+func _on_exit_button_pressed():
+	print("Goodbye")
+	get_tree().quit()
