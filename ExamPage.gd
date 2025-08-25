629c79221978e2f2d0f2754ff36b5db03910a259
IntelektualusMarozas <juozassadauskas2@gmail.com>
2025-08-25 16:39:18 +0300

    fix: dead doc popup window

--- ExamPage.gd
+++ ExamPage.gd
@@ -1,33 +1,12 @@
-extends VBoxContainer
+extends Node2D
 
-@onready var feedback_label = $FeedbackLabel
-@onready var feedback_timer = $FeedbackTimer
-@onready var help_button = $HelpButton
-@onready var help_popup = $HelpPopup
+@onready var help_button = $CanvasLayer/HelpButton
+@onready var help_popup = $CanvasLayer/HelpPopup
 
 func _ready():
-	# Connect to the ExamManager's signal when the page loads
 	help_button.pressed.connect(_on_help_button_pressed)
-	ExamManager.feedback_provided.connect(_on_feedback_provided)
-	feedback_timer.timeout.connect(_on_feedback_timer_timeout)
-
-func _on_feedback_provided(message, is_correct):
-	feedback_label.text = message
-	
-	if is_correct:
-		feedback_label.modulate = Color.GREEN
-	else:
-		feedback_label.modulate = Color.RED
-		
-	# Show the label and start the timer to hide it after a delay
-	feedback_label.show()
-	feedback_timer.start()
-
-func _on_feedback_timer_timeout():
-	# Hide the label when the timer finishes
-	feedback_label.hide()
 
 func _on_help_button_pressed():
-	print("DOCS button pressed")
+	print("Help button was pressed!")
 	help_popup.set_image("res://images/formulės_1.jpg")
 	help_popup.popup()
